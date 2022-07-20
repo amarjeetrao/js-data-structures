c943a205a25280dc26f5860c98cab248cd7f98d9
Amarjeet <amarjeet.billfree@gmail.com>
2022-07-20 17:41:55 +0530

    linkedlist based stack

---
+++ stack/linkedListStack.js
@@ -0,0 +1,100 @@
+// Stack is a linear data structure which follows a particular order in which the operations are performed.
+// The order may be LIFO(Last In First Out).
+
+// Implementing it with LinkedList
+
+// Basic Operations
+// push() − Pushing (storing) an element on the stack. [time - O(1), space - O(1)]
+// pop() − Removing (accessing) an element from the stack. [time - O(1), space - O(1)]
+// peek() − get the top data element of the stack, without removing it. [time - O(1), space - O(1)]
+// isFull() − check if stack is full.
+// isEmpty() − check if stack is empty.
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor(length) {
+		this.first = null;
+		this.last = null;
+		this.length = length;
+		this.position = -1;
+	}
+
+	size() {
+		return this.length;
+	}
+
+	isFull() {
+		return this.length == this.position;
+	}
+
+	isEmpty() {
+		return this.position == -1;
+	}
+
+	push(data) {
+		if (!this.isFull()) {
+			const node = new Node(data);
+			if (this.isEmpty()) {
+				//case if list is empty
+				this.first = node;
+				this.last = node;
+			} else {
+				let temp = this.first;
+				this.first = node;
+				node.next = temp;
+			}
+			return ++this.position;
+		}
+		return this.position;
+	}
+
+	pop() {
+		if (this.isEmpty()) {
+			return -1;
+		}
+		this.first = this.first.next;
+		return --this.position;
+	}
+
+	peek() {
+		return this.first.value;
+	}
+
+	print() {
+		if (this.isEmpty()) {
+			console.log('List is Empty');
+			return null;
+		}
+
+		let listValues = '';
+		let current = this.first;
+		while (current) {
+			listValues += current.value + ' ';
+			current = current.next;
+		}
+		return listValues;
+	}
+}
+
+//sample data
+const stack = new Stack(10);
+console.log(stack.isEmpty());
+
+stack.push(2);
+stack.push(5);
+stack.push(7);
+stack.push(1);
+stack.push(4);
+stack.push(33);
+stack.push(44);
+console.log(stack.push(6));
+console.log(stack.size());
+console.log(stack.print());
+console.log(stack.pop());
+console.log(stack.peek());
